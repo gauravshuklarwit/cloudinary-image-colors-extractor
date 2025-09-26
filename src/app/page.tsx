87ba59e122a72b7057083073188ca60bb1b7d6f8
Gaurav Shukla <shukla.gaurav@rwit.io>
2025-09-26 12:45:37 +0530

    feat: add project details on the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import LogoColorPicker from "@/components/logo-color-picker";
 export default function Home() {
   return (
     <main>
-      <LogoColorPicker />
+      <div className="container mx-auto px-5">
+        <div className="text-center my-10 space-y-3">
+          <h1 className="text-3xl font-bold">Image Color Extractor</h1>
+          <p className="text-sm text-pretty">Using Cloudinary API to extract colors from logo images.</p>
+        </div>
+        <LogoColorPicker />
+      </div>
     </main>
   );
 }
